1e55fc68d72dae9fb8b7c78b83fa94f4fb35486d
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-15 17:37:04 +0700

    File structure

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# File structure
+"""
+|_  sql_app
+    |_  __init__.py
+    |_  crud.py
+    |_  database.py
+    |_  main.py
+    |_  models.py
+    |_  schemas.py
+"""
